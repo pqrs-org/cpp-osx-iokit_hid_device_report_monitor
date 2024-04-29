93e933f7e372090e41e5875b6fadddc16c018c81
Takayama Fumihiko <tekezo@pqrs.org>
2024-04-29 15:46:33 +0900

    Version 1.1.0

--- include/pqrs/osx/iokit_hid_device_report_monitor.hpp
+++ include/pqrs/osx/iokit_hid_device_report_monitor.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::iokit_hid_device_report_monitor v1.0
+// pqrs::osx::iokit_hid_device_report_monitor v1.1
 
 // (C) Copyright Takayama Fumihiko 2022.
 // Distributed under the Boost Software License, Version 1.0.
